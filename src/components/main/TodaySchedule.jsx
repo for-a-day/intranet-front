0b9d6dc08197dd637cf490f2cd28adeeeafe38b2
pmk <pmkna@naver.com>
2024-07-08 10:25:10 +0900

    홈에서 부서별 디폴트 캘린더에서 오늘의 일정만 불러오게 하기

--- src/components/main/TodaySchedule.jsx
+++ src/components/main/TodaySchedule.jsx
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {
-  Card,
-  CardContent,
-  Typography,
-  Box,
-  Menu,
-  MenuItem,
-  IconButton,
-} from "@mui/material";
-import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import { Card, CardContent, Typography, Box, Menu, MenuItem, IconButton } from "@mui/material";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
@@ -23,6 +15,7 @@ const options = ["Action", "Another Action", "Something else here"];
 const TodaySchedule = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [activities, setActivities] = useState([]);
+  const [departmentCode, setDepartmentCode] = useState("");
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -32,35 +25,62 @@ const TodaySchedule = () => {
   };
 
   useEffect(() => {
-    const fetchTodayActivities = async () => {
+    const userData = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const response = await axios.get("http://localhost:9000/app/employees/token", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          const employee = response.data.employee;
+          setDepartmentCode(employee.department.departmentCode);
+        } catch (error) {
+          console.error("유저 정보 못불러옴", error);
+        }
+      }
+    };
+
+    userData();
+  }, []);
+
+  useEffect(() => {
+    const fetchTodayActivities = async (code) => {
       try {
-        const response = await axios.get("http://localhost:9000/app/schedule/32");
+        const response = await axios.get(`http://localhost:9000/app/schedule/${code}`);
         const schedules = response.data.schedule;
-        const today = new Date().toLocaleDateString("ko-KR", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        }).replace(/\. /g, '-').replace(/\./g, '').trim();
+        console.log(`${code}`);
+        console.log("왜안뜸", schedules);
+        const today = new Date()
+          .toLocaleDateString("ko-KR", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          })
+          .replace(/\. /g, "-")
+          .replace(/\./g, "")
+          .trim();
 
         const todayActivities = schedules
-          .filter(
-            (schedule) =>
-              new Date(schedule.startDate).toISOString().split("T")[0] === today
-          )
+          .filter((schedule) => new Date(schedule.startDate).toISOString().split("T")[0] === today)
           .map((schedule) => ({
             time: schedule.startTime.slice(0, 5),
             color: "primary.main",
             text: schedule.subject,
           }));
-        console.log("today", todayActivities);
         setActivities(todayActivities);
       } catch (error) {
         console.error("Error fetching schedules:", error);
       }
     };
 
+    if (departmentCode !== null) {
+      fetchTodayActivities(departmentCode);
+    }
+
     fetchTodayActivities();
-  }, []);
+  }, [departmentCode]);
 
   return (
     <Card>
